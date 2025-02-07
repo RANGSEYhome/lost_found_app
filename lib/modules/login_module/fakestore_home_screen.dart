d9283d56211c3c38bcd2f21d1d7b70115ff24d1d
Chongeang YOU <chongeangy@gmail.com>
2025-02-07 15:31:12 +0700

    update profile display

--- lib/modules/login_module/fakestore_home_screen.dart
+++ lib/modules/login_module/fakestore_home_screen.dart
@@ -5,12 +5,13 @@ import 'package:lost_found_app/core/localization/lang_logic.dart';
 import 'package:lost_found_app/core/localization/lang_data.dart';
 // modules
 import 'package:lost_found_app/modules/login_module/fakestore_loading_screen.dart';
+import 'package:lost_found_app/modules/login_module/fakestore_login_models.dart';
 import 'package:provider/provider.dart';
 import 'fakestore_login_logic.dart';
 
 class FakestoreHomeScreen extends StatefulWidget {
-  const FakestoreHomeScreen({super.key});
-
+ // const FakestoreHomeScreen({super.key});
+  
   @override
   State<FakestoreHomeScreen> createState() => _FakestoreHomeScreenState();
 }
@@ -23,7 +24,21 @@ class _FakestoreHomeScreenState extends State<FakestoreHomeScreen> {
     _lang = context.watch<LanguageLogic>().lang;
     _langIndex = context.watch<LanguageLogic>().langIndex;
     return Scaffold(
-      appBar: AppBar(title: Text("Account")),
+      appBar: AppBar(title: Text("Account"),
+      actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.logout),
+            tooltip: 'Show Snackbar',
+            onPressed: () async {
+              await context.read<FakestoreLoginLogic>().clear();
+              Navigator.of(context).pushReplacement(
+                CupertinoPageRoute(
+                    builder: (context) => FakeStoreLoadingScreen()),
+              );
+            },
+          ),
+        ],
+      ),
       body: _buildBody(),
     );
   }
@@ -31,6 +46,7 @@ class _FakestoreHomeScreenState extends State<FakestoreHomeScreen> {
   Widget _buildBody() {
     return ListView(
       children: [
+        _buildProfile(),
         ListTile(
         leading: Icon(
           Icons.logout,
@@ -46,6 +62,85 @@ class _FakestoreHomeScreenState extends State<FakestoreHomeScreen> {
         },
       ),
       ],
+    );
+  }
+  Widget _buildProfile(){
+    MyResponseModel responseModel =
+              context.watch<FakestoreLoginLogic>().responseModel;
+  // String imgProfile = '${responseModel.user?.profilePic ?? 'https://cdn-icons-png.flaticon.com/512/149/149071.png'}';
+  // print("ste" + imgProfile);
+  String imgProfile = '${responseModel.user?.profilePic}';
+if (imgProfile == null || imgProfile.isEmpty) {
+  imgProfile = 'https://cdn-icons-png.flaticon.com/512/149/149071.png';
+}
+
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+        side: BorderSide(color: Colors.green),
+      ),
+      color: Colors.grey[300],
+      elevation: 4,
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Profile Image
+            CircleAvatar(
+              radius: 40,
+              backgroundImage: NetworkImage(
+                imgProfile
+                //"https://cdn-icons-png.flaticon.com/512/149/149071.png"
+              ),
+            ),
+            SizedBox(width: 16),
+
+            // User Info
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "${responseModel.user?.firstname ?? 'No Name'}",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.green),
+                ),
+                SizedBox(height: 8),
+                Row(
+                  children: [
+                    Icon(Icons.person, color: Colors.green, size: 18),
+                    SizedBox(width: 8),
+                    Text('male', style: TextStyle(fontSize: 16, color: Colors.green)),
+                  ],
+                ),
+                SizedBox(height: 8),
+                Row(
+                  children: [
+                    Icon(Icons.phone, color: Colors.green, size: 18),
+                    SizedBox(width: 8),
+                    Text('0000000000001', style: TextStyle(fontSize: 16, color: Colors.green)),
+                  ],
+                ),
+                SizedBox(height: 8),
+                Row(
+                  children: [
+                    Icon(Icons.email, color: Colors.green, size: 18),
+                    SizedBox(width: 8),
+                    Text('example@gmail.com', style: TextStyle(fontSize: 16, color: Colors.green)),
+                  ],
+                ),
+              ],
+            ),
+
+            Spacer(),
+
+            // Edit Icon
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.edit, color: Colors.green),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
