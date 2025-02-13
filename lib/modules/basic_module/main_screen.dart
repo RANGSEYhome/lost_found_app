9c98bf6b637643401082c025340b9d78bfc28b8d
rangseyhome <info.rangsey@gmail.com>
2025-02-13 12:53:15 +0700

    Refactor MainScreen state management and navigation logic

--- lib/modules/basic_module/main_screen.dart
+++ lib/modules/basic_module/main_screen.dart
@@ -33,14 +33,15 @@ class _MainScreenState extends State<MainScreen> {
   int _currentIndex = 0;
   // const Home_Screen({super.key});
   final _scaffoldKey = GlobalKey<ScaffoldState>();
-  
- @override
+
+  @override
   void initState() {
     super.initState();
-     context.read<FakestoreLoginLogic>().responseModel;
-     context.read<FakestoreLoginLogic>().read(); 
+    context.read<FakestoreLoginLogic>().responseModel;
+    context.read<FakestoreLoginLogic>().read();
     // context.read<PostLogic>().read();
   }
+
   @override
   Widget build(BuildContext context) {
     _lang = context.watch<LanguageLogic>().lang;
@@ -62,16 +63,20 @@ class _MainScreenState extends State<MainScreen> {
               child: IconButton(
                 icon: Icon(Icons.add_circle_outline),
                 onPressed: () {
-                   FakestoreLoginLogic loginLogic = Provider.of<FakestoreLoginLogic>(context, listen: false);
-                        MyResponseModel responseModel = loginLogic.responseModel;
-             //   final responseModel = context.watch<FakestoreLoginLogic>().responseModel;
+                  FakestoreLoginLogic loginLogic =
+                      Provider.of<FakestoreLoginLogic>(context, listen: false);
+                  MyResponseModel responseModel = loginLogic.responseModel;
+                  //   final responseModel = context.watch<FakestoreLoginLogic>().responseModel;
                   if (responseModel.token == null) {
                     print("responseModel.tokens: ${responseModel.token}");
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => FakeStoreLoginScreen()),
-                    );
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(
+                    //       builder: (context) => FakeStoreLoginScreen()),
+                    // );
+                    setState(() {
+                      _currentIndex = 2;
+                    });
                   } else {
                     debugPrint("responseModel.token: ${responseModel.token}");
                     Navigator.push(
